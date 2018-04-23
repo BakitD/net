e9475324e3c9157363e78f98e6417831de55366b
bakit <bakitds@gmail.com>
2018-04-23 13:54:14 +0600

    Added initial client and server. Updated gitingore file

---
+++ src/Client.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"net"
+)
+
+
+const (
+	PORT = "9999"
+)
+
+
+func main() {
+
+
+
+	os.Exit(0)
+}
---
+++ src/Server.go
@@ -0,0 +1,11 @@
+package main
+
+
+const (
+	PORT = 10999
+)
+
+
+func main() {
+
+}

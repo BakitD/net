5992eb28b0b970324299797199df468d3b2c93d8
bakit <bakitds@gmail.com>
2018-04-24 14:23:36 +0600

    Updated client and server

--- src/client/client.go
+++ src/client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io"
+	"os"
 	"fmt"
 	"net"
+	"path/filepath"
 )
 
 var SERVERS []string = []string{":9999"}
@@ -19,23 +22,59 @@ func connect(address string) (conn *net.TCPConn, err error) {
 	return conn, nil
 }
 
-func start(data string) {
-	var buffer []byte = make([]byte, 512)
+func find_file(filename string, conn net.Conn) (int, error) {
+	return conn.Write([]byte(filename))
+}
 
+func receive_file(filename string, conn net.Conn) (int, error) {
+	var bytes_written int = 0
+	var buffer []byte = make([]byte, BUFFER_LENGTH)
+
+	path := filepath.Join(SAVE_FILE_TO_DIR, filepath.Base(filename))
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return bytes_written, err
+	}
+
+	defer f.Close()
+
+	for {
+		bytes_read, err := conn.Read(buffer)
+		if err != nil && err != io.EOF {
+			return bytes_written, err
+		}
+		if bytes_read == 0 {
+			break
+		} else {
+			n, err := f.Write(buffer[:bytes_read])
+			if err != nil {
+				return bytes_written + n, err
+			}
+			bytes_written += n
+		}
+	}
+	return bytes_written, nil
+}
+
+
+func start(filename string) {
 	for _, server := range SERVERS {
 		conn, err := connect(server)
 		if err == nil {
-			conn.Write([]byte(data))
-			for {
-				bytes_read, _ := conn.Read(buffer)
-				if bytes_read == 0 {
-					break
+			_, err = find_file(filename, conn)
+			if err != nil {
+				print_error(err)
+			} else {
+				bytes_received, err := receive_file(filename, conn)
+				if err != nil {
+					print_error(err)
 				} else {
-					fmt.Println(string(buffer[:bytes_read]))
+					print_message(fmt.Sprintf("%d bytes were received", bytes_received))
+					break
 				}
 			}
 		} else {
-			fmt.Println(err)
+			print_error(err)
 		}
 
 	}
---
+++ src/client/constants.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	ERROR_MESSAGE = "Error %s"
+	ERROR_CODE_AT_EXIT = 1
+	BUFFER_SIZE = 512
+	BUFFER_READ_SIZE = 512
+)
+
+const (
+	BUFFER_LENGTH = 512
+	SAVE_FILE_TO_DIR = "/home/bakit/projects/nets/files/input"
+)
